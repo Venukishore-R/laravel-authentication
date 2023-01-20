49ddc1331be7e93f49118b73ed99a2add49f95c9
Venukishore-R <venuvk0304@gmail.com>
2023-01-20 12:32:20 +0530

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,38 @@
+<!-- <style>
+a:link, a:visited {
+  background-color: #f44336;
+  color: white;
+  padding: 14px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:hover, a:active {
+  background-color: red;
+}
+</style> -->
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    You're logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div >
+        @if(Auth::user()->role == 'admin')
+        <a href="{{url('/adminpage')}}" target="_blank">Admin page</a>
+        @endif
+    </div>
+</x-app-layout>
